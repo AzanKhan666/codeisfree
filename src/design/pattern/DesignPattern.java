b9e015bcc85503960e14bec8b7cd44d767ff2be9
AzaN <AzaN@192.168.10.4>
2020-10-09 09:01:40 +0500

    heloo is my 3nd riprosetry

--- src/design/pattern/DesignPattern.java
+++ src/design/pattern/DesignPattern.java
@@ -15,6 +15,7 @@ public class DesignPattern {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");    }
+        System.out.println("hello");
+System.out.println("hello2");    }
     
 }
